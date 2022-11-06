2ad6fd4b1ab9dab61bad3fd422e432457e92aaae
KaziNasir <shoumikkazi@gmail.com>
2022-11-06 18:10:10 +0600

    renting preprocess added

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -1,4 +1,4 @@
-@extends('customer.layout.ap')
+@extends('customer.layout.app')
 @section('heading', 'Dashboard')
 
 @section('main_content')
@@ -7,13 +7,16 @@
         <div class="section-header">
             <h1>HomePage</h1>
         </div>
+        <a href="{{ route("myProducts") }}"><button type="submit" class="btn btn-primary btn-lg btn-block">
+            My Products
+        </button></a>
+        <br>
+        <a href="{{ route("rentProduct") }}"><button type="submit" class="btn btn-primary btn-lg btn-block">
+            Rent Products
+        </button></a>
     </div>
     <div class="col-lg-4 col-md-6 col-sm-6 col-12">
         <div class="card card-statistic-1">
-            <a href="{{ route("myProducts") }}"><button type="submit" class="btn btn-primary btn-lg btn-block">
-                Viwe Products
-            </button></a>
-            
         </div>
     </div>
 </div>
