19fbf6a56d82b3d0f992272929cbca77e3d5dd3e
Mehadi Hasan Reaz <65062761+MehadiReaz@users.noreply.github.com>
2022-11-06 21:42:25 +0600

    photosize

--- resources/views/product/editProduct.blade.php
+++ resources/views/product/editProduct.blade.php
@@ -54,7 +54,8 @@
                                     @endif
                                 </div>
                                 <div class="col-md-3">
-                                    <img src="{{ asset('uploads/product/'.$product->photo)}}" alt="" >
+                                    <img src="{{ asset('uploads/product/'.$product->photo)}}" alt="" style="max-width: 200px; m-height: 100px;"
+                                    >
                                     <input type="file" name="photo" value="{{$product->photo}}" >
                                 </div>
                                 <div class="mb-3">
