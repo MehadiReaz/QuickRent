d3cd76fb3c8dc893bf798679c18fd917fcc2e5eb
hamim44 <abrarhamim4044h@gmail.com>
2022-12-04 20:08:55 +0600

    token migration

---
+++ database/migrations/2022_12_04_132323_create_tokens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->id();
+            $table->integer('c_id');
+            $table->string('token');
+            $table->datetime('created_at');
+            $table->datetime('expired_at');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tokens');
+    }
+};
